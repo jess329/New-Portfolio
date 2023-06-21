40894190d53dc670e5fc87acecb0af6b08014b10
jasper <jasper.mittag@outlook.de>
2023-06-22 00:54:09 +0200

    Hero

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './Navbar'
+import Hero from './Hero'
+import "./Responsive.css"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,11 +12,11 @@ function App() {
   return (
     <body>
       <header>
-        
+        <Navbar/>
       </header>
 
       <main>
-
+        <Hero/>
       </main>
 
       <footer>
---
+++ src/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+        <h3 className='name'>jasper</h3>
+        <div className='navbar-links'>
+            <h3>About</h3>
+            <h3>Work</h3>
+            <h3>Hit me up!</h3>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
+
